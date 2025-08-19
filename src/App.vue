aac09f8ab2f6476ce67d66551e167a6408dfd5c9
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-19 15:44:50 +0300

    remove useless inports and styles, Import Footer and header components

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+import Header from '@/components/layouts/TheHeader.vue';
+import Footer from '@/components/layouts/TheFooter.vue';
+
+const menu = ref([
+  {
+    id: '',
+    title: '',
+    description: '',
+    type: '',
+    price: '',
+    size: ['S', 'M', 'L'],
+    components: ['coffe', 'milk']
+  }
+])
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+ <Header />
   <main>
-    <TheWelcome />
   </main>
+  <Footer />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>

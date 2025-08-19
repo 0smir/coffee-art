19a4ca86508b332d2f7cd31570c2e1f56abb3e21
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-19 15:40:33 +0300

    add base components button and svg-icon

---
+++ src/components/base-components/BaseButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+const props = defineProps({
+  classList: {
+    type: String,
+    reqired: false
+  }
+})
+
+</script>
+
+<template>
+  <button :class="['btn', classList]">
+    <slot></slot>
+  </button>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/base-components/SvgIcon.vue
@@ -0,0 +1,59 @@
+<template>
+  <component :is="icon"></component>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  iconType: {
+    type: String,
+    required: false
+  }
+});
+let icon = null;
+if (props.iconType) {
+  icon = defineAsyncComponent(() => import(`@/assets/images/icons/${props.iconType}/${props.name}.svg`));
+} else {
+  icon = defineAsyncComponent(() => import(`@/assets/images/icons/${props.name}.svg`));
+}
+
+
+
+</script>
+
+<style lang="scss">
+.icon {
+  width: 24px;
+  height: 24px;
+  pointer-events: none;
+
+  &--large {
+    width: 30px;
+    height: 30px;
+  }
+
+  &--medium {
+    width: 18px;
+    height: 18px;
+  }
+
+  &--small {
+    width: 15px;
+    height: 15px;
+  }
+
+  &>svg {
+    object-fit: contain;
+  }
+
+  &--logo {
+    transform: rotate(45deg);
+  }
+
+}
+</style>

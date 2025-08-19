2dcdef9937ac44e2ff0fde736f06091845a2f3e5
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-19 15:43:38 +0300

    add components and some logic

---
+++ src/components/CoffeeItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import RatingLabel from '@/components/RatingLabel.vue';
+const rating = ref(5)
+
+</script>
+
+<template>
+  <div class="product-item">
+    <RatingLabel :rating="rating" />
+    <div class="product-item__img-wrapper"><img src="" alt=""></div>
+
+    
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/RatingLabel.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import SvgIcon from '@/components/base-components/SvgIcon.vue';
+
+const props = defineProps({
+  rating: {
+    type: Number,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <div class="rating__label">
+    <SvgIcon class="icon" name="star" /> {{ rating }} 
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>

f2d19f25a0b987b30995209f890bf986cb13e3f8
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-19 15:32:45 +0300

    add components for header and footer

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="/menu">Menu</a></li>
+      <li><a href="/delivery">Delivery</a></li>
+    </ul>
+  </header>
+</template>
+
+
+
+<style scoped>
+
+</style>

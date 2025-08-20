09cc6f89117e6093c18ee7b1b45bd3463e9f8fa9
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-20 11:30:44 +0300

    add menu object and import components

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,65 @@ import { ref } from 'vue';
 import Header from '@/components/layouts/TheHeader.vue';
 import Footer from '@/components/layouts/TheFooter.vue';
 
+import CoffeeItem from '@/components/CoffeeItem.vue';
+
+
+
 const menu = ref([
   {
-    id: '',
-    title: '',
-    description: '',
+    id: '001',
+    title: 'Espresso ',
+    description: 'Rich and bold shot of pure coffee',
+    img: ['', ''],
     type: '',
-    price: '',
+    price: '3.25',
     size: ['S', 'M', 'L'],
-    components: ['coffe', 'milk']
-  }
+    components: ['coffe'],
+    rating: 4.7
+  },
+  {
+    id: '002',
+    title: 'Americano',
+    description: 'Espresso topped with hot water',
+    img: ['', ''],
+    type: '',
+    price: '3.53',
+    size: ['S', 'M', 'L'],
+    components: ['coffe', 'water'],
+    rating: 5
+  },
+  {
+    id: '003',
+    title: 'Cappuccino',
+    description: 'Espresso with steamed milk and thick milk foam',
+    img: ['', ''],
+    type: '',
+    price: '4.50',
+    size: ['S', 'M', 'L'],
+    components: ['coffe', 'milk'],
+    rating: 4.9
+  },
+  {
+    id: '004',
+    title: 'Latte',
+    description: 'Espresso with steamed milk, light foam on top',
+    img: ['', ''],
+    type: '',
+    price: '4.53',
+    size: ['S', 'M', 'L'],
+    components: ['coffe', 'water', 'milk'],
+    rating: 4.5
+  },
+  // {
+  //   id: '',
+  //   title: '',
+  //   description: '',
+  //   img: ['', ''],
+  //   type: '',
+  //   price: '',
+  //   size: ['S', 'M', 'L'],
+  //   components: ['coffe', 'water']
+  // }
 ])
 
 </script>
@@ -21,6 +70,12 @@ const menu = ref([
 <template>
  <Header />
   <main>
+    <ul class="product-list">
+      <li class="product-list__item" v-for="product in menu">
+        <CoffeeItem :key="product.id" :product="product" />
+      </li>
+    </ul>
+    
   </main>
   <Footer />
 </template>

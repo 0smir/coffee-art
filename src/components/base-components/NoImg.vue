042e333460976d6776a1f84ced0374c2c02a64ff
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-21 22:10:06 +0300

    add component to use it as a dummy img  block

---
+++ src/components/base-components/NoImg.vue
@@ -0,0 +1,27 @@
+<script setup>
+import SvgIcon from '@/components/base-components/SvgIcon.vue';
+</script>
+
+<template>
+  <div class="dummy_img">
+    <SvgIcon class="icon" name="photo"/>
+
+  </div>
+</template>
+
+
+
+<style lang="scss">
+.dummy_img {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: auto;
+  color: $gray;
+  &>.icon {
+    width: 50px;
+    height: 50px;
+  }
+}
+</style>

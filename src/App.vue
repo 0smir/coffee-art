c7d4bda43e6bc086ee56bf0cb61f3d1fa175c074
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-21 20:33:40 +0300

    remove redundant code, import menu data, add styles, change mark up

--- src/App.vue
+++ src/App.vue
@@ -5,77 +5,32 @@ import Header from '@/components/layouts/TheHeader.vue';
 import Footer from '@/components/layouts/TheFooter.vue';
 
 import CoffeeItem from '@/components/CoffeeItem.vue';
+import {MENU} from '@/appData.js';
 
 
 
-const menu = ref([
-  {
-    id: '001',
-    title: 'Espresso ',
-    description: 'Rich and bold shot of pure coffee',
-    img: ['', ''],
-    type: '',
-    price: '3.25',
-    size: ['S', 'M', 'L'],
-    components: ['coffe'],
-    rating: 4.7
-  },
-  {
-    id: '002',
-    title: 'Americano',
-    description: 'Espresso topped with hot water',
-    img: ['', ''],
-    type: '',
-    price: '3.53',
-    size: ['S', 'M', 'L'],
-    components: ['coffe', 'water'],
-    rating: 5
-  },
-  {
-    id: '003',
-    title: 'Cappuccino',
-    description: 'Espresso with steamed milk and thick milk foam',
-    img: ['', ''],
-    type: '',
-    price: '4.50',
-    size: ['S', 'M', 'L'],
-    components: ['coffe', 'milk'],
-    rating: 4.9
-  },
-  {
-    id: '004',
-    title: 'Latte',
-    description: 'Espresso with steamed milk, light foam on top',
-    img: ['', ''],
-    type: '',
-    price: '4.53',
-    size: ['S', 'M', 'L'],
-    components: ['coffe', 'water', 'milk'],
-    rating: 4.5
-  },
-  // {
-  //   id: '',
-  //   title: '',
-  //   description: '',
-  //   img: ['', ''],
-  //   type: '',
-  //   price: '',
-  //   size: ['S', 'M', 'L'],
-  //   components: ['coffe', 'water']
-  // }
-])
+const menu = ref(MENU);
 
 </script>
 
 <template>
  <Header />
   <main>
-    <ul class="product-list">
-      <li class="product-list__item" v-for="product in menu">
-        <CoffeeItem :key="product.id" :product="product" />
-      </li>
-    </ul>
-    
+    <div class="container">
+      <ul class="products-list">
+        <li class="products-list__item" v-for="product in menu">
+          <CoffeeItem :key="product.id" :product="product" />
+        </li>
+      </ul>
+    </div>
   </main>
   <Footer />
 </template>
+
+<style lang="scss">
+.products-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
+}
+</style>

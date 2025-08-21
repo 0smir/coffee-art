8a29f2bd733df10c61c132e48648b46288b3d120
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-21 20:48:08 +0300

    add additional props: floated, theme, add corrects to mark up: additional classes, complite styles for dark and light themes

--- src/components/RatingLabel.vue
+++ src/components/RatingLabel.vue
@@ -6,19 +6,62 @@ const props = defineProps({
   rating: {
     type: Number,
     required: true
+  },
+  floated: {
+    type: Boolean,
+    default: false,
+    required: false
+  },
+  theme: {
+    type: String,
+    default: 'dark',
+    required: false
   }
 })
 
 </script>
 
 <template>
-  <div class="rating__label">
-    <SvgIcon class="icon" name="star" /> {{ rating }} 
+  <div :class="['rating__label', {'floated': floated},  theme !== 'dark' ? 'light' : 'dark']">
+    <SvgIcon  v-if="!rating" class="icon icon--medium icon--oulined" name="star-outlined" /> 
+    <SvgIcon  v-else class="icon icon--medium icon--filled" name="star" /> 
+    {{ rating }} 
   </div>
 </template>
 
 
 
-<style lang="scss" scoped>
+<style lang="scss">
+.rating__label {
+  display: flex;
+  align-items: center;
+  padding: 5px 3px;
+ 
+  &.floated {
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding: 5px 5px 5px 10px;
+    font-size: 14px;
+    font-weight: 300;
+  }
 
+  &.dark {
+    color: $white;
+    border-bottom-left-radius: $radius_large;
+    background-color: rgba($black, .2);
+  }
+
+  &.light {
+    background-color: transparent;
+  }
+
+  .icon {
+    margin-right: 5px;
+    color: $gold;
+  }
+  .icon--oulined {
+    color: $gold;
+  }
+}
 </style>

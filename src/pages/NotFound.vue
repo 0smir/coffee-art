90c3871ca1a6e6e0300d27c9c57c74987675020d
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-25 22:07:34 +0300

    add link to home page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,12 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
 <template>
   <div class="page page--404">
     <h1>NotFound</h1>
+    <router-link to="/">Home</router-link>
   </div>
 </template>
 
-<script setup>
 
-</script>
 
 <style lang="scss" scoped>
 

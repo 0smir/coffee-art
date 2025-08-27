604c0d37904d30b781b7fef8403e2096f7d584b1
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-27 23:18:15 +0300

    add corrects for product size param - size as object

--- src/appData.js
+++ src/appData.js
@@ -9,12 +9,12 @@ export const MENU = [
   {
     id: '001',
     title: 'Espresso ',
-    description: 'Rich and bold shot of pure coffee',
+    description: 'Rich and bold shot of pure coffee. A small but very strong coffee made by forcing hot water through finely-ground coffee beans under high pressure. It’s the base for many other coffee drinks.',
     img: [espresso],
     type: 'hot',
     base: 'espresso',
     price: '3.25',
-    size: ['S', 'M'],
+    size: [{ id: 's-001-s', size: 'S' }, { id: 's-001-m', size: 'M' }],
     components: ['coffe'],
     rating: 4.7,
     voutes: 635
@@ -22,12 +22,12 @@ export const MENU = [
   {
     id: '002',
     title: 'Americano',
-    description: 'Espresso topped with hot water',
+    description: 'Espresso topped with hot water. Espresso diluted with hot water, resulting in a milder taste and larger cup compared to straight espresso. Similar strength to drip coffee but with espresso’s aroma.',
     img: [americano],
     type: 'hot',
     base: 'espresso',
     price: '3.53',
-    size: ['S', 'M', 'L'],
+    size: [{ id: 's-002-s', size: 'S' }, { id: 's-002-m', size: 'M' }, { id: 's-002-l', size: 'L' }],
     components: ['coffe', 'water'],
     rating: 5,
     voutes: 115
@@ -35,12 +35,12 @@ export const MENU = [
   {
     id: '003',
     title: 'Cappuccino',
-    description: 'Espresso with steamed milk and thick milk foam',
+    description: 'Espresso with steamed milk and thick milk foam. Made with equal parts espresso, steamed milk, and milk foam. It has a rich coffee flavor balanced with creamy texture and a thick, airy foam on top.',
     img: [cappuccino],
     type: 'hot',
     base: 'deep foam',
     price: '4.50',
-    size: ['S', 'M'],
+    size: [{ id: 's-003-s', size: 'S' }, { id: 's-003-m', size: 'M' }],
     components: ['coffe', 'milk'],
     rating: 4.9,
     voutes: 35
@@ -48,12 +48,12 @@ export const MENU = [
   {
     id: '004',
     title: 'Latte',
-    description: 'Espresso with steamed milk, light foam on top',
+    description: 'Espresso with steamed milk, light foam on top. Espresso with a larger amount of steamed milk and just a thin layer of milk foam. It’s smooth, creamy, and less intense than cappuccino.',
     img: [latte],
     type: 'hot',
     base: 'microfoam',
     price: '4.53',
-    size: ['M', 'L'],
+    size: [{ id: 's-004-m', size: 'M' }, { id: 's-004-l', size: 'L' }],
     components: ['coffe', 'water', 'milk'],
     rating: 4.5,
     voutes: 53
@@ -61,12 +61,12 @@ export const MENU = [
   {
     id: '005',
     title: 'Iced Latte',
-    description: 'Espresso poured over ice with chilled milk',
+    description: 'Espresso poured over ice with chilled milk. A chilled version of latte: espresso poured over cold milk and ice. Refreshing, smooth, and great for warm days.',
     img: [icelatte],
     type: 'cold',
     base: 'microfoam',
     price: '4.25',
-    size: ['M', 'L'],
+    size: [{ id: 's-005-m', size: 'M' }, { id: 's-005-l', size: 'L' }],
     components: ['coffe', 'water', 'milk', 'ice'],
     rating: 0,
     voutes: 0
@@ -74,12 +74,12 @@ export const MENU = [
   {
     id: '006',
     title: 'Bumble Coffee',
-    description: 'A refreshing iced espresso drink made with honey and orange juice, poured over ice',
+    description: 'A refreshing iced espresso drink made with honey and orange juice, poured over ice. A trendy drink made by layering espresso over sparkling orange juice with honey (sometimes other citrus or syrups are added). Sweet, citrusy, and energizing with a unique taste contrast between coffee bitterness and fresh juice.',
     img: [bumble],
     type: 'cold',
     base: 'microfoam',
     price: '4.5',
-    size: ['M', 'L'],
+    size: [{ id: 's-006-m', size: 'M' }, { id: 's-006-l', size: 'L' }],
     components: ['coffe', 'honey', 'orange juice', 'ice'],
     rating: 4.9,
     voutes: 150

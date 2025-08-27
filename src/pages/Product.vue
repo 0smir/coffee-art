b0efcfd38578bbb1dcbff94a1d70c3947cca2745
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-08-27 23:35:28 +0300

    add content for product page

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,22 +1,35 @@
 <script setup>
 import { useRoute } from 'vue-router'
+import { ref, computed } from 'vue';
+import ProductItemDetails from '@/components/ProductItemDetails.vue'
+import {MENU} from '@/appData.js'
 
-const route = useRoute()
-// const props = defineProps({
-//   id: {
-//     type: String,
-//     require: true,
-//   }
-// });
-console.log(route.params.id)
+const route = useRoute();
+const menu = ref(MENU);
+const id = route.params.id;
+
+const product = computed(() => {
+  return menu.value.find(item => item.id == id) ?? null;
+});
+
 </script>
 
 <template>
   <div class="page page--product-details">
-    <h1>Product Item {{ route.params.id }}</h1>
+    <div class="container">
+      <ProductItemDetails v-if="product" :product="product"/>
+      <p v-else>Product not found.</p>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.page--product-details{
+  padding-top: 20px;
+
+  @media(min-width: $sm){
+    padding-top: 40px;
+  }
+}
 
 </style>
